8f3c1e4df7e7ed8e3387d995c269c7b877f36e4b
Lee Ingabo <ustadhlee17@gmail.com>
2022-03-16 13:06:27 +0300

    first commit

---
+++ precache-manifest.0ae7518967d29a4c2f9a3c3be6a49519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d7a77da691eb1f4ae7ae14d8bbe077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014a5e7562408358fb9d",
+    "url": "/static/js/2.13c22267.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.13c22267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf587d481e046cb7bac",
+    "url": "/static/js/main.a050fdcf.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "7b5674ecc5f1e06d413b14dd4d780a46",
+    "url": "/static/media/commerce.7b5674ec.png"
+  }
+]);
